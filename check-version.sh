8a423c9482027a38bb94dab2704207e927b75e4e
Romain Hunault <romain.hunault@e.email>
2020-06-24 15:14:37 +0000

    Don't use PRODUCT_VERSION_MAINTENANCE

--- check-version.sh
+++ check-version.sh
@@ -12,8 +12,7 @@ VENDOR_COMMIT_ID=$(cat default.xml | grep $VENDOR | sed -E 's/.*revision="(.*)" 
 curl -s https://gitlab.e.foundation/e/os/$VENDOR/-/raw/$VENDOR_COMMIT_ID/config/common.mk -o common.mk
 PRODUCT_VERSION_MAJOR=$(cat common.mk | grep "PRODUCT_VERSION_MAJOR = " | sed -E 's/PRODUCT_VERSION_MAJOR = (.*)/\1/')
 PRODUCT_VERSION_MINOR=$(cat common.mk | grep "PRODUCT_VERSION_MINOR = " | sed -E 's/PRODUCT_VERSION_MINOR = (.*)/\1/')
-PRODUCT_VERSION_MAINTENANCE=$(cat common.mk | grep "PRODUCT_VERSION_MAINTENANCE := " | sed -E 's/PRODUCT_VERSION_MAINTENANCE := (.*)/\1/')
-PRODUCT_VERSION="$PRODUCT_VERSION_MAJOR.$PRODUCT_VERSION_MINOR.$PRODUCT_VERSION_MAINTENANCE"
+PRODUCT_VERSION="$PRODUCT_VERSION_MAJOR.$PRODUCT_VERSION_MINOR"
 echo "Product version: $PRODUCT_VERSION"
 
 COMMIT_TAG_VERSION=$(echo $CI_COMMIT_TAG | sed -E 's/v(.\..\..).*/\1/')
