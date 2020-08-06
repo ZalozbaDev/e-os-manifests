5fa7b6419398cab5c12dbae5c77ba16036e2b142
Romain Hunault <romain.hunault@e.email>
2020-08-06 18:29:28 +0200

    Handle version number with a 0 as last character

--- check-version.sh
+++ check-version.sh
@@ -10,12 +10,12 @@ fi
 VENDOR_COMMIT_ID=$(cat default.xml | grep $VENDOR | sed -E 's/.*revision="(.*)" .*/\1/')
 
 curl -s https://gitlab.e.foundation/e/os/$VENDOR/-/raw/$VENDOR_COMMIT_ID/config/common.mk -o common.mk
-PRODUCT_VERSION_MAJOR=$(cat common.mk | grep "PRODUCT_VERSION_MAJOR = " | sed -E 's/PRODUCT_VERSION_MAJOR = (.*)/\1/')
-PRODUCT_VERSION_MINOR=$(cat common.mk | grep "PRODUCT_VERSION_MINOR = " | sed -E 's/PRODUCT_VERSION_MINOR = (.*)/\1/')
+PRODUCT_VERSION_MAJOR=$(cat common.mk | grep "PRODUCT_VERSION_MAJOR = " | sed -E 's/PRODUCT_VERSION_MAJOR = ([0-9]*)/\1/')
+PRODUCT_VERSION_MINOR=$(cat common.mk | grep "PRODUCT_VERSION_MINOR = " | sed -E 's/PRODUCT_VERSION_MINOR = ([0-9]*)/\1/')
 PRODUCT_VERSION="$PRODUCT_VERSION_MAJOR.$PRODUCT_VERSION_MINOR"
 echo "Product version: $PRODUCT_VERSION"
 
-COMMIT_TAG_VERSION=$(echo $CI_COMMIT_TAG | sed -E 's/v(.\..).*/\1/')
+COMMIT_TAG_VERSION=$(echo $CI_COMMIT_TAG | sed -E 's/v([0-9]*\.[0-9]*).*/\1/')
 echo "Tag version:     $COMMIT_TAG_VERSION"
 
 
