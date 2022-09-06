563a80301b6ea22a10db125135fe66f9fa110768
Daniel Zoba <daniel.sobe@sorben.com>
2022-09-06 09:45:08 +0200

    remember which container was used to build this version

--- build.sh
+++ build.sh
@@ -10,3 +10,7 @@ docker run -v "/srv/e/src:/srv/src" -v "/srv/e/zips:/srv/zips" -v "/srv/e/logs:/
 -e "BRANCH_NAME=devel_fp3-q" -e "DEVICE_LIST=FP3" -e "REPO=https://github.com/ZalozbaDev/e-os-manifests.git"  \
 -e "OTA_URL=https://eosupdate.serbski-inkubator.de/api" \
 registry.gitlab.e.foundation:5000/e/os/docker-lineage-cicd:community
+
+# to build with a known-good container, use this one
+## zalozbadev/e_os_docker-lineage-cicd:v0_17_1_q
+
