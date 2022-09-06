80ebfcf1f8d12ad9691cbe601b6fd3c5c48a7f6a
Daniel Zoba <daniel.sobe@sorben.com>
2022-09-06 09:51:02 +0200

    adhere to some builtin branch naming convention

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ mkdir -p /srv/e/src /srv/e/zips /srv/e/logs /srv/e/ccache
 
 # customized
 docker run -v "/srv/e/src:/srv/src" -v "/srv/e/zips:/srv/zips" -v "/srv/e/logs:/srv/logs" -v "/srv/e/ccache:/srv/ccache" \
--e "BRANCH_NAME=devel_fp3-q" -e "DEVICE_LIST=FP3" -e "REPO=https://github.com/ZalozbaDev/e-os-manifests.git"  \
+-e "BRANCH_NAME=v1.3.1-devel-q" -e "DEVICE_LIST=FP3" -e "REPO=https://github.com/ZalozbaDev/e-os-manifests.git"  \
 -e "OTA_URL=https://eosupdate.serbski-inkubator.de/api" \
 registry.gitlab.e.foundation:5000/e/os/docker-lineage-cicd:community
