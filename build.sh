962e7c4f66c990d2ca7b0b8dec4b4433f1de7afe
Daniel Zoba <daniel.sobe@sorben.com>
2024-02-22 09:13:04 +0100

    port files from old branch

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p /srv/e/src /srv/e/zips /srv/e/logs /srv/e/ccache
+
+# original
+# docker run -v "/srv/e/src:/srv/src" -v "/srv/e/zips:/srv/zips" -v "/srv/e/logs:/srv/logs" -v "/srv/e/ccache:/srv/ccache" -e "BRANCH_NAME=v0.17.1-q" -e "DEVICE_LIST=FP3" -e "REPO=https://gitlab.e.foundation/e/os/releases.git"   registry.gitlab.e.foundation:5000/e/os/docker-lineage-cicd:community
+
+# customized
+docker run -v "/srv/e/src:/srv/src" -v "/srv/e/zips:/srv/zips" -v "/srv/e/logs:/srv/logs" -v "/srv/e/ccache:/srv/ccache" \
+-e "BRANCH_NAME=v1.3.1-devel-q" -e "DEVICE_LIST=FP3,starlte" -e "REPO=https://github.com/ZalozbaDev/e-os-manifests.git"  \
+-e "OTA_URL=https://eosupdate.serbski-inkubator.de/api" \
+docker-lineage-cicd-custom:latest
+
+# to run interactively w/o starting a build, add this:
+## --it --entrypoint /bin/bash
+
